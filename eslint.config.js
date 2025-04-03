551973fd2880b1bb13b8c16e089ee4a49a097cf1
Azeem Mirza <azeem.mirza94@gmail.com>
2025-04-03 09:56:00 -0500

    chore: add ESLint configuration for TypeScript with Prettier integration

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+const js = require('@eslint/js');
+const tsEslint = require('typescript-eslint');
+const prettier = require('eslint-config-prettier');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    ignores: ['node_modules', 'dist', 'coverage', '*.test.ts'],
+  },
+  js.configs.recommended,
+  ...tsEslint.configs.strictTypeChecked,
+  prettier,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsEslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsEslint.plugin,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+      eqeqeq: ['error', 'always'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+    },
+  },
+  {
+    files: ['src/**/*.test.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
